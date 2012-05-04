0f0c9dc633aee1dd28dc1fddb8f5ed7858ba523d
Alexander Jahraus <fanaugen@me.com>
2012-05-04 02:43:26 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Valency::Application.config.secret_token = '3daffdc0dcca7b279e2602e98f11cddf281cfebe8f559fcbfe91665a82b5aabb073f226e5cf7b3cafba9c1ed94b4ac9df7effc5b1f7a81f6dfd6d510c8f1f7ab'
