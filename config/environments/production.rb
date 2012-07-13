bce1a1ae5e380861ab94559c4744c8da2d2ad2d6
Alexander Jahraus <fanaugen@me.com>
2012-07-13 09:48:29 +0200

    Deploy very preliminary online version

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,9 @@ Valency::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  # Secure Heroku environment with password:
+  config.middleware.insert_after(::Rack::Lock, "::Rack::Auth::Basic", "Production") do |u, p|
+      [u, p] == [ENV['PREVIEW_USERNAME'], ENV['PREVIEW_PASSWORD']]
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -64,7 +64,7 @@ Valency::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'languages#index'
 
   # See how all your routes lay out with "rake routes"
 end
