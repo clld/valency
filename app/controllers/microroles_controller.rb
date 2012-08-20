ca69ae995774d02d3cee826b72afba417cd77ed3
Alexander Jahraus <fanaugen@me.com>
2012-08-20 23:04:41 +0200

    Add Microroles Controller and Views

---
+++ app/controllers/microroles_controller.rb
@@ -0,0 +1,24 @@
+class MicrorolesController < ApplicationController
+  # GET /microroles
+  # GET /microroles.json
+  def index
+    @microroles = Microrole.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @microroles }
+    end
+  end
+
+  # GET /microroles/1
+  # GET /microroles/1.json
+  def show
+    @microrole = Microrole.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @microrole }
+    end
+  end
+
+end
---
+++ test/unit/helpers/microroles_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class MicrorolesHelperTest < ActionView::TestCase
+end
