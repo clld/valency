1708242114645f2c3b954c569ee8b8c7d81a1d8e
Alexander Jahraus <fanaugen@me.com>
2012-10-31 15:15:37 +0100

    wip improve top navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,13 +4,13 @@ module ApplicationHelper
     # If a @language is specified, then this must be
     # the controller of a language-specific resource
     # otherwise, just link to the language
-    no_other_controller = @language.nil? || controller_name == 'languages'
+    link_to_controller = @language && controller_name == 'languages'
     @languages.map do |lang|
       css_class = 'active' if lang == @language
       link_href = no_other_controller ? lang : send('language_' << controller_name + '_path', lang)
       capture do
         content_tag(:li, class: css_class) do
-          link_to lang, link_href
+          link_to lang, link_href, title: controller_name.humanize
         end
       end
     end.join("\n").html_safe
