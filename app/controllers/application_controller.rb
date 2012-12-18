a23764b0d88af1a8eb851105bb979b50671feaac
Alexander Jahraus <fanaugen@me.com>
2012-12-18 15:32:05 +0100

    remove basic HTTP auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :global_queries
   
-  http_basic_authenticate_with :name => ENV['HTTP_USER'], :password => ENV['HTTP_PASSWORD'], :realm => 'see LinguaWiki'
-  
   # necessary for languages dropdown,
   # meanings dropdown and more
   def global_queries
