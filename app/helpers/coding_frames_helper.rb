f81365d877394e1012818a70f9634c2e5edebe11
Alexander Jahraus <fanaugen@me.com>
2013-02-27 11:17:37 +0100

    finish coding frame index template

--- app/helpers/coding_frames_helper.rb
+++ app/helpers/coding_frames_helper.rb
@@ -22,8 +22,8 @@ module CodingFramesHelper
   # render a coding frame, optionally with a prefixed badge and a hidden <span>arg_count</span>
   # wrap all numbers in <b class="idx-no" data-idx-no="1">
   # addClass("label") dynamically to highlight the labels
+  # options can include "link" (boolean), defaults to false
   def render_cf cf, options = {}
-    link      = options[:link]      || true  # link to the Coding frame
     prefix    = options[:prefix]    || false # prefix a 'B'/'D' label for Basic/Derived?
     highlight = options[:highlight] || EMPTY_ARY  # [2] highlight index no. 2
     tooltip   = options[:tooltip]   || EMPTY_HASH # {2 => "tooltip for 2"}
@@ -37,10 +37,12 @@ module CodingFramesHelper
     cf_with_markup.gsub!(/(V'?)/, '<b>\\1</b>')  # make the verb bold
     cf_with_markup.gsub!(/(\[)?(\d+)(\])?/) do |num|
       css_class = "idx-no" # short for index number
-      css_class << " free" # free means not in brackets. Required for stylesheet and dynamic highlighting
       n = $2.to_i
       # add class "label", for highlighted numbers, except in brackets
-      css_class << " label" if highlight.include?(n) && $1 != '['
+      if $1 != '['
+        css_class << " free" # for stylesheet and dynamic highlighting
+        css_class << " label" if highlight.include?(n)
+      end
       if tooltip_text = tooltip[n]
         css_class << " ttip"
         html_attr = " rel='tooltip' title='#{tooltip_text}'"
@@ -48,7 +50,7 @@ module CodingFramesHelper
       "#{$1}<b class='#{css_class}' data-idx-no='#{n}'#{html_attr}>#{n}</b>#{$3}"
     end
     
-    cf_html = link_to_if(link && @language, cf_with_markup.html_safe, [@language, cf])
+    cf_html = link_to_if(options[:link] && @language, cf_with_markup.html_safe, [@language, cf])
     cf_html = "#{prefix}<div class='cell'>#{cf_html}</div>" if prefix
     div_for(cf, :'data-arg-count' => cf.arg_count) {cf_html.html_safe}
     
