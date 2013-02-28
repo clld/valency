addbe89b0f2aaea114f56cbd8882a12daf1b0169
Alexander Jahraus <fanaugen@me.com>
2013-02-28 08:36:03 +0100

    improve Meaning#show: add filter
    filter preset for dataTable: filter by number of participants in basic
    Coding frame

--- app/controllers/meanings_controller.rb
+++ app/controllers/meanings_controller.rb
@@ -16,6 +16,11 @@ class MeaningsController < ApplicationController
     # TODO remove the bang in production
     @meaning = Meaning.find_by_label_for_url!(params[:id])
     @microroles = @meaning.microroles.all
+    @verbs      = @meaning.verbs.includes(:coding_frame)
+    @arg_counts = @verbs.map do |v|
+      cf = v.coding_frame
+      cf.arg_count unless cf.nil? || cf.coding_frame_schema.blank?
+    end.uniq.delete_if{|x|x.nil?}.sort!
 
     respond_to do |format|
       format.html # show.html.erb
