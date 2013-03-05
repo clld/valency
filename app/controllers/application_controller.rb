e108a545fd81c90a0faff5b626b6e55c18cf5090
Alexander Jahraus <fanaugen@me.com>
2013-03-05 23:41:00 +0100

    add HTTP basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :global_queries
   
+  # password protection: basic HTTP auth
+  http_basic_authenticate_with(
+    :name     => ENV['HTTP_USER'],
+    :password => ENV['HTTP_PASSWORD'],
+    :realm    => 'see LinguaWiki'
+  )
   # necessary for languages dropdown,
   # meanings dropdown and more
   def global_queries
