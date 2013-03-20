19e3a011474e078b65511db0183752ec2f41e5d5
Alexander Jahraus <fanaugen@me.com>
2013-03-20 12:31:05 +0100

    update contributors' photos

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,2 +1,10 @@
 module PeopleHelper
+  
+  # returns a Hash of the form {person:Person => photo_asset:Sprockets::StaticAsset or nil}
+  def get_photo_assets people
+    Hash[ people.map do |person|
+      [ person, Rails.application.assets.find_asset("photos/#{person.id}.jpg") ]
+    end]
+  end
+  
 end
