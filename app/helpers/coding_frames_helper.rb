f2a079a52c070f8ae26130622fbbb470790000ae
Hans-Jörg Bibiko <bibiko@eva.mpg.de>
2013-10-25 00:47:09 +0200

    fixed exception if a coding frame value is missing for a verb

--- app/helpers/coding_frames_helper.rb
+++ app/helpers/coding_frames_helper.rb
@@ -43,30 +43,32 @@ module CodingFramesHelper
       "<span class=\"label\" title=\"#{cf.derived? ? 'Derived' : 'Basic'} coding frame\">"<<
       "#{cf.derived? ? 'D' : 'B'}</span></div>"
     end
-    cf_html = cf.to_s.gsub  /</, LT  # replace HTML special symbols
-              cf_html.gsub! />/, GT
-    cf_html.gsub!(/(V'?)/, '<b>\\1</b>') # make V or V' bold
-    cf_html.gsub!(/(\(.+?\)|\[.+?\])/, '<span>\\1</span>') # no linebreak (in brackets)
-    cf_html.gsub!(/(\d+)/) do |num|
-      n = $1.to_i  # $1 is the number
-      css_class = 'idx-no' # for index number
-      css_class << ' highlight' if highlight.include?(n)
-      if tooltip_text = tooltip[n]
-        css_class << ' ttip'
-        html_attr = " rel=\"tooltip\" title=\"#{tooltip_text}\""
+    if !cf.to_s.nil?
+      cf_html = cf.to_s.gsub  /</, LT  # replace HTML special symbols
+      cf_html.gsub! />/, GT
+      cf_html.gsub!(/(V'?)/, '<b>\\1</b>') # make V or V' bold
+      cf_html.gsub!(/(\(.+?\)|\[.+?\])/, '<span>\\1</span>') # no linebreak (in brackets)
+      cf_html.gsub!(/(\d+)/) do |num|
+        n = $1.to_i  # $1 is the number
+        css_class = 'idx-no' # for index number
+        css_class << ' highlight' if highlight.include?(n)
+        if tooltip_text = tooltip[n]
+          css_class << ' ttip'
+          html_attr = " rel=\"tooltip\" title=\"#{tooltip_text}\""
+        end
+        "<b class=\"#{css_class}\" data-idx-no=\"#{n}\"#{html_attr}>#{$1}</b>"
       end
-      "<b class=\"#{css_class}\" data-idx-no=\"#{n}\"#{html_attr}>#{$1}</b>"
-    end
-    cf_html.gsub!(/'/, APOSTROPHE)
-    cf_html = cf_html.html_safe
+      cf_html.gsub!(/'/, APOSTROPHE)
+      cf_html = cf_html.html_safe
     
-    cf_html = link_to_if(options[:link], cf_html, language_coding_frame_path(cf.language, cf)) do 
-      content_tag(:a, cf_html)
+      cf_html = link_to_if(options[:link], cf_html, language_coding_frame_path(cf.language, cf)) do 
+        content_tag(:a, cf_html)
+      end
+      cf_html = "#{prefix}<div class=\"cell\">#{cf_html}</div>" if prefix
+      div_for(cf, :'data-arg-count' => cf.arg_count, :class => options[:css_class]) do
+        cf_html.html_safe
     end
-    cf_html = "#{prefix}<div class=\"cell\">#{cf_html}</div>" if prefix
-    div_for(cf, :'data-arg-count' => cf.arg_count, :class => options[:css_class]) do
-      cf_html.html_safe
-    end
+  end
     
   end
   
