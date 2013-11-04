79c0bf22f3bc21f58feffa9fea158f520aadd0d5
Hans-Jörg Bibiko <bibiko@eva.mpg.de>
2013-11-04 19:21:13 +0100

    updated languageref model

--- app/models/languageref.rb
+++ app/models/languageref.rb
@@ -1,6 +1,6 @@
 class Languageref < ActiveRecord::Base
   self.primary_key = :id
-#  attr_accessible :family, :id, :iso_code, :name, :variety, :alternation_occurs_judgement_criteria, :characterization_of_flagging_resources, :characterization_of_indexing_resources, :characterization_of_ordering_resources, :comments, :continent, :latitude, :longitude, :name_for_url, :data_sources_generalizations_contributor_backgrounds
+#  attr_accessible , :id, :name, :nodata
 
   has_many :alternations    ,:dependent => :destroy
   has_many :coding_frames   ,:dependent => :destroy
@@ -13,10 +13,7 @@ class Languageref < ActiveRecord::Base
   has_many :contributors, through: :contributions, source: :person
   
   default_scope order(:name)
-  
-  # generate helper methods for gmaps4rails
-  acts_as_gmappable process_geocoding: false
-  
+
   # allows using just @language instead of @language.name in views
   def to_s
     name
