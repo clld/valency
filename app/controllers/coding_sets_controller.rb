076e7c71560cd75a3dfa6cb15a8e5f2ef6ed9246
Hans-Jörg Bibiko <bibiko@eva.mpg.de>
2013-11-04 22:37:35 +0100

    further work on coding_sets - not yet finished

--- app/controllers/coding_sets_controller.rb
+++ app/controllers/coding_sets_controller.rb
@@ -9,10 +9,9 @@ class CodingSetsController < ApplicationController
   # GET /languages/ainu/coding_sets[.json]
   def index
     @coding_sets = @language.coding_sets.includes(:coding_frame_index_numbers => :coding_frame)
-    
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @coding_frames }
+      format.json { render json: @coding_sets }
     end
   end
 
@@ -21,9 +20,15 @@ class CodingSetsController < ApplicationController
     @coding_set = @language.coding_sets.includes(:coding_frame_index_numbers => [:coding_frame, :microroles]).find(params[:id])
     @coding_frames = @coding_set.coding_frames
 
+    # @cf_index_numbers = @coding_frames.coding_frame_index_numbers.includes(:microroles, :coding_set)
+    # 
+    # @verbs   = @coding_frames.related_verbs.includes(:meanings)
+    # verb_ids = @coding_frames.related_verb_ids
+
+
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @coding_frame }
+      format.json { render json: @coding_sets }
     end
   end
   
